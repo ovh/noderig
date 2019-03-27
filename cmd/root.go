0ccaa766d9a25ae841a8b1baa6dac253f2cac43e
Aurelien Hebert <aurelien.hebert@corp.ovh.com>
2019-03-27 15:33:39 +0100

    feat(collectors): add keep-metrics, which allow to always returned the last value fetched

--- cmd/root.go
+++ cmd/root.go
@@ -70,6 +70,7 @@ func initConfig() {
 
 	// Defaults
 	viper.SetDefault("flushPeriod", 10000)
+	viper.SetDefault("keep-metrics", false)
 
 	// Bind environment variables
 	viper.SetEnvPrefix("noderig")
@@ -248,7 +249,11 @@ func getCollectors() []core.Collector {
 			}
 
 			for _, file := range files {
-				disk := collectors.NewCollector(path.Join(dir.Name(), file.Name()), uint(interval), uint(viper.GetInt("keep-for")))
+				disk := collectors.NewCollector(
+					path.Join(dir.Name(), file.Name()),
+					uint(interval),
+					uint(viper.GetInt("keep-for")),
+					viper.GetBool("keep-metrics"))
 				cs = append(cs, disk)
 			}
 		}
